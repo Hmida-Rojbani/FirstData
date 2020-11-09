3d0238a49e387ea42f548bc3476e557a17868f7e
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-11-09 15:42:10 +0100

    Update TelephoneNumberEntity.java

--- src/main/java/de/tekup/rest/data/models/TelephoneNumberEntity.java
+++ src/main/java/de/tekup/rest/data/models/TelephoneNumberEntity.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -22,6 +23,5 @@ public class TelephoneNumberEntity {
 	private String operator;
 	
 	@ManyToOne
-	@JsonIgnore
 	private PersonEntity person;
 }
